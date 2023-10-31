d6751c2095999c389e10a13e6fb6ecb49dc9e16c
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2023-10-31 10:52:35 +0100

    Generate random background color for each paragraph calculating light or darkbg

--- Create/script.js
+++ Create/script.js
@@ -3,10 +3,36 @@ const textContents = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca",
 "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", 
 "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"];
 
+const getRandomColor = () => {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  };
+
+  function isLight(backgroundColor) {
+    // Calculate the brightness of the background color
+    const rgb = backgroundColor.match(/\d+/g);
+    const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+    return brightness > 128;
+  }
+
 textContents.forEach(content => {
   const newSection = document.createElement('section');
   const newParagraph = document.createElement('p');
+  const randomBackgroundColor = getRandomColor();
+
+  newParagraph.style.backgroundColor = randomBackgroundColor;
+
+  if (isLight(randomBackgroundColor)) {
+    newParagraph.style.color = 'black';
+  } else {
+    newParagraph.style.color = 'white';
+  }
+
   newParagraph.appendChild(document.createTextNode(content));
   newSection.appendChild(newParagraph);
   newArticle.appendChild(newSection);
 });
+
+

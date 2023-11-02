5b1ee5570c4486db487e2ed8885c48d143295160
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2023-11-02 11:06:41 +0100

    Put last child of ol tag at beginning of list

---
+++ Navigate/script.js
@@ -0,0 +1,16 @@
+const olElement = document.querySelector('section:first-of-type ol');
+
+if (olElement) {
+  const lastListItem = olElement.lastElementChild;
+  olElement.insertBefore(lastListItem, olElement.firstElementChild);
+}
+
+
+const secondH2 = document.querySelector('section:nth-of-type(2) h2');
+const thirdH2 = document.querySelector('section:nth-of-type(3) h2');
+
+if (secondH2 && thirdH2) {
+  const tempText = secondH2.textContent;
+  secondH2.textContent = thirdH2.textContent;
+  thirdH2.textContent = tempText;
+}

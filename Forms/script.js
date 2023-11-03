40f5fab00fa6aed322a0fac2c8ecef7039f25b16
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2023-11-03 10:22:14 +0100

    Complete exersises for Forms

--- Forms/script.js
+++ Forms/script.js
@@ -1,8 +1,53 @@
-//TODO: Add a keyup listener on the first input field so that feild input goes into <span id="display-firstname">
-const inputField = document.getElementById("firstname");
-const displayFirstName = document.getElementById("display-firstname");
+const inputFirstname = document.getElementById("firstname");
+const displayFirstname = document.getElementById("display-firstname");
 
-inputField.addEventListener("keyup", function {
-  const inputText = inputField.value;
-  displayFirstName.textContent = inputText;
+inputFirstname.addEventListener("keyup", function() {
+ 
+  displayFirstname.textContent = inputFirstname.value;
+});
+
+const ageInput = document.getElementById("age");
+const hardTruthSection = document.getElementById("a-hard-truth");
+
+ageInput.addEventListener("keyup", function() {
+  const ageValue = ageInput.value.trim();
+  const age = parseInt(ageValue, 10);
+
+  if (ageValue === "") {
+    hardTruthSection.style.visibility = "hidden";
+  } else {
+    if (age < 18) {
+      hardTruthSection.style.visibility = "hidden";
+    } else {
+      hardTruthSection.style.visibility = "visible";
+    }
+  }
+});
+
+const passwordInput = document.getElementById("pwd");
+const passwordConfirmInput = document.getElementById("pwd-confirm");
+
+function validatePassword() {
+  const password = passwordInput.value;
+  const passwordConfirm = passwordConfirmInput.value;
+  const isShort = password.length < 6;
+  const doNotMatch = password !== passwordConfirm && passwordConfirm !== "";
+
+  passwordInput.style.borderColor = isShort || doNotMatch ? "red" : "";
+  passwordConfirmInput.style.borderColor = doNotMatch ? "red" : "";
+}
+
+passwordInput.addEventListener("keyup", validatePassword);
+passwordConfirmInput.addEventListener("keyup", validatePassword);
+
+const toggleDarkMode = document.getElementById("toggle-darkmode");
+
+toggleDarkMode.addEventListener("change", function() {
+  if (toggleDarkMode.value === "dark") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
 });
